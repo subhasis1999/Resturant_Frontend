0e39081003a5b765613b35bd291042f5d0843b4b
subhasis1999 <subhasissahoo6866@gmail.com>
2026-01-05 00:15:24 +0530

    Initial frontend setup

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -132,7 +132,7 @@ const Reservation = () => {
     try {
       const { data } = await axios.post(
         // "http://localhost:4000/api/v1/reservation/send",
-        "https://resturant-backend-sigma.vercel.app/api/v1/reservation/send",
+        "https://resturant-backend-ruddy.vercel.app/api/v1/reservation/send",
         {
           firstName,
           lastName,
